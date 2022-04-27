646cfd22a14aaac3828ee350f83dca382996e204
n1kford <n1kf0rd@mail.ru>
2022-04-27 09:20:31 +0600

    version 1.08

--- app.js
+++ app.js
@@ -41,12 +41,14 @@ app.use('*', (req, res) => {
   res.status(404).send({ message: 'Ошибка: данный ресурс на найден.' });
 });
 
+app.use(errors());
+
 // eslint-disable-next-line no-unused-vars
 app.use((err, req, res, next) => {
   const { statusCode = 500, message } = err;
   if (err.code === 11000) {
     res.status(409).send({ message: 'Ошибка: пользователь с таким e-mail уже существует.' });
-  } else if (err.name === 'ValidationError' || err.name === 'CastError' || err.name === 'TypeError' || err.message === 'Validation failed') {
+  } else if (err.name === 'ValidationError' || err.name === 'CastError' || err.name === 'TypeError') {
     res.status(400).send({ message: 'Ошибка: данные переданы неккоректно' });
   } else if (err.message === 'Ошибка: Неправильные почта или пароль.') {
     res.status(401).send({ message: err.message });
@@ -54,7 +56,5 @@ app.use((err, req, res, next) => {
     res.status(statusCode).send({ message: statusCode === 500 ? 'Ошибка: что-то пошло не так.' : message });
   }
 });
-
-app.use(errors());
 
 app.listen(PORT);
