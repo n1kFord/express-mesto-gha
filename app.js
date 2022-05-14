7873b8c8c279f7d81b5aa7041f1ded2eee1f3f13
n1kford <n1kf0rd@mail.ru>
2022-05-14 15:34:59 +0600

    version 2.08

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(requestLogger);
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Conrol-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
   next();
 });
 
